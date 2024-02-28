8c5197d4fd91960255a2551b4bf3dcacc745d79e
PriscillaB <priscillatech@outlook.com>
2024-02-28 11:21:51 -0300

    navbar footer e home

---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,27 @@
+
+import React from 'react'
+
+
+function Navbar() {
+ 
+  
+
+  return (
+    <>
+     <div className='w-full bg-green-500 text-white flex justify-center py-4'>
+          <div className="container flex justify-between text-lg">
+            <div className='text-2xl font-bold uppercase'>Farmacia de Todos</div>
+
+            <div className='flex gap-4'>
+              <div className='hover:underline'>Produtos</div>
+              <div className='hover:underline'>Categorias</div>
+              <div className='hover:underline'>Cadastrar Categoria</div>
+              <div className='hover:underline'>Sair</div>
+            </div>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
